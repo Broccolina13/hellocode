1b03273a312132ae88dc6323f26c1c67feec072a
Broccolina13 <boyarskaya-m@inbox.ru>
2020-10-11 20:18:07 +0300

    !

--- start.java
+++ start.java
@@ -2,6 +2,7 @@ package com.javarush.test;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Программистами не рождаются");
+        System.out.println("Программистами не рождаются!");
+
     }
 }
